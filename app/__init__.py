9148ea1348f5bd3233c37847569d1d166584a906
Nahom kiflu <kiflunahom1994@gmail.com>
2024-08-13 16:23:48 +0300

    fix __init__.py from app folder(forgot to initialize the db

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,12 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
+from config import Config
 from app.blacklist import blacklist
 
+db = SQLAlchemy()
+bcrypt = Bcrypt()
 jwt = JWTManager()
 
 def create_app(config_class=Config):
